edafb67133e3e3fa51e64e5c125289585c8b81dd
Agus969 <agus.tin.2024@outlook.com>
2024-09-02 10:17:02 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+double x , y , resultado;
+cout<<"Enter two numbers : ";
+cin >> x >> y;
+//calculo matematico
+resultado= x* y ;
+cout<<"Resultado = "<< resultado<< endl;
+return 0;
+}
